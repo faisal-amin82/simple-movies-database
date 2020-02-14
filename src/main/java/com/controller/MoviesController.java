725e805ffa2b914e03b79b9306f805eeb5bf0f22
Faisal Amin <famin@companieshouse.gov.uk>
2020-02-14 17:52:01 +0000

    Added media type

--- src/main/java/com/controller/MoviesController.java
+++ src/main/java/com/controller/MoviesController.java
@@ -3,6 +3,7 @@ package com.controller;
 import com.model.movies.Movies;
 import com.service.MoviesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/simple-movies-db")
+@RequestMapping(path = "/simple-movies-db", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MoviesController {
 
     @Autowired
     private MoviesService moviesService;
 
     private static final String PATH = "/Users/famin/MoviesDB/simple-movies-database/src/main/resources/static/movies.json";
-    @GetMapping(value = "/getAllMovies")
+    @GetMapping(path = "/getAllMovies")
     public List<Movies> getAll() {
 
 
